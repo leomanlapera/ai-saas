5cb3182e8615c1901940ac9865b69067eed22d8a
June Leoman Lapera <leomanlapera@gmail.com>
2025-05-29 15:05:02 +0800

    basic testing for authentication user creation and user login

--- env.ts
+++ env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string().min(1),
+  BETTER_AUTH_SECRET: z.string().min(1),
+  BETTER_AUTH_URL: z.string().min(1),
 });
 
 export const parsedEnv = envSchema.parse(process.env);
